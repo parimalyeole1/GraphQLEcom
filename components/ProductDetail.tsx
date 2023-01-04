a7a29fa54af73c2e779e8bbc6efef29339138e00
Parimal Yeole <parimal.yeole@moduscreate.com>
2023-01-04 14:51:33 +0530

    Add products to cart

--- components/ProductDetail.tsx
+++ components/ProductDetail.tsx
@@ -1,7 +1,13 @@
+import { getCookie } from "cookies-next";
 import { Product } from "../lib/products";
+import { GetCartDocument, useAddToCartMutation } from "../types";
 import { ProductItem } from "./ProductItem";
 
 export function ProductDetail({ product }: { product: Product | null }) {
+  const cartId = String(getCookie("cartId"));
+  const [addToCart, { loading }] = useAddToCartMutation({
+    refetchQueries: [GetCartDocument],
+  });
   if (!product) {
     return null;
   }
@@ -10,9 +16,32 @@ export function ProductDetail({ product }: { product: Product | null }) {
       <div className="col-span-3 flex items-center justify-center">
         <ProductItem product={product} />
       </div>
-      <div className="space-y-4 p-8">
+      <form
+        className="space-y-4 p-8"
+        onSubmit={(e) => {
+          e.preventDefault();
+          addToCart({
+            variables: {
+              input: {
+                cartId,
+                id: product.id,
+                name: product.title,
+                description: product.body,
+                price: product.price,
+                image: product.src,
+              },
+            },
+          });
+        }}
+      >
         <div dangerouslySetInnerHTML={{ __html: product.body }} />
-      </div>
+        <button
+          className="w-full rounded border border-black bg-black px-6 py-4 uppercase text-white hover:bg-white hover:text-black"
+          type="submit"
+        >
+          {loading ? "Adding to cart..." : "Add to cart"}
+        </button>
+      </form>
     </main>
   );
 }
